4e429f272551b556966f2fa482428503685144fd
AhmetEminOzturk <eminahmetozturk@gmail.com>
2023-08-16 02:29:08 +0300

    u

--- React-1/proj1/src/index.js
+++ React-1/proj1/src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+
 
 //import pages
 import Home from './pages/Home'
+import MasterPage from './pages/MasterPage';
+import Aboutus from './pages/Aboutus';
+
+const router =
+    <BrowserRouter>
+        <Routes>
+            <Route path='/' element={<MasterPage item={<Home />} />}/>
+            <Route path='/about' element={<MasterPage item={<Aboutus />} />}/>
+        </Routes>
+    </BrowserRouter>
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <Home />
-);
-
+root.render( router );
--- React-1/proj1/src/pages/Home.js
+++ React-1/proj1/src/pages/Home.js
@@ -1,19 +1,9 @@
 import React from 'react'
-import { arr, appTitle } from '../datas/product'
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
-import Menu from '../components/Menu'
+import { arr } from '../datas/product'
 
 function Home() {
     return (
         <>
-            <Navbar />
-            <div className='row mt-2'>
-                <div className='col-xs-12 col-sm-12 col-md-12 col-lg-3 col-xl-3 col-xxl-3'>
-                    <Menu />
-                </div>
-                <div className='col-xs-12 col-sm-12 col-md-12 col-lg-9 col-xl-9 col-xxl-9'>
-                    <h1>{appTitle}</h1>
                     <div className="row">
                         {arr.map((item, index) =>
                             <div key={index} className='mb-2 col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4'>
@@ -35,9 +25,6 @@ function Home() {
                             </div>
                         )}
                     </div>
-                </div>
-            </div>
-            <Footer />
         </>
     )
 }
