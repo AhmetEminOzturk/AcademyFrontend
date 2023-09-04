7d76da3408e28cf914f6c870dbb57ab505ff7a9c
AhmetEminOzturk <eminahmetozturk@gmail.com>
2023-09-04 15:22:14 +0300

    Update Dashboard.tsx

--- ReactProjects/proj2/src/pages/Dashboard.tsx
+++ ReactProjects/proj2/src/pages/Dashboard.tsx
@@ -1,6 +1,14 @@
 import React from 'react'
 
 function Dashboard() {
+
+    const stData = localStorage.getItem("user")
+    
+    if (stData !== null) {
+        const obj = JSON.parse(stData)
+        console.log(obj.email , obj.name , obj.phone)    
+    }
+    
   return (
     <div>Dashboard</div>
   )
