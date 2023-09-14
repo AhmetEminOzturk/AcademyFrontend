8aec95e1e4b5cb05b3956e5b79f1e3d9aca0c21b
AhmetEminOzturk <eminahmetozturk@gmail.com>
2023-09-14 09:32:00 +0300

    u

--- ReactProjects/portal/src/pages/Search.tsx
+++ ReactProjects/portal/src/pages/Search.tsx
@@ -1,18 +1,21 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../components/Header'
 import NavBar from '../components/NavBar'
 import { useParams } from 'react-router-dom'
 import { search } from '../Api'
+import ProductItem from '../components/ProductItem'
+import { IProducts } from '../models/IProducts'
 
 function Search() {
 
     const {q} = useParams()
+    const [proObj, setProObj] = useState<IProducts>()
     useEffect(() => {
         if(q){
             search(q,0).then(res=> {
             const dt = res.data
             if(dt){
-                console.log(dt)
+                setProObj(dt)
             }
       })
         } 
@@ -23,6 +26,11 @@ function Search() {
     <Header/>
     <NavBar/>
     <h2>Search Result : {q}</h2>
+    <div className="row">
+        {proObj && proObj.products.map((item, index) =>
+          <ProductItem item={item} key={index} />
+        )}
+      </div>
     </>
   )
 }
